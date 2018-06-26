96cf6fe03a8c9d4046afbb1543f4b69e23c5430a
Ryan <rmshivers42@gmail.com>
2018-06-26 13:54:32 -0400

    Update CryptoCompare.py

--- CryptoCompare.py
+++ CryptoCompare.py
@@ -10,7 +10,7 @@ class CryptoCompare(object):
 	
 	
     def query(self, url):
-        req = urllib2.Request(url)
+        req = urllib2.Request(url)AKIAIEPBDDPKANL7QZMQ
         response = json.loads(urllib2.urlopen(req).read())
         
         if(type(response) != "None"):
